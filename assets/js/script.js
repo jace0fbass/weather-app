98c046e1f2e8f4edea6f335839c72d17a0abf069
Jason <jajones414@gmail.com>
2022-10-08 15:54:04 -0500

    I have updated the formatting of the project.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd MMMM Do, YYYY"));
 console.log(currentDay.format("dddd MMMM Do, YYYY"));
@@ -15,7 +14,7 @@ console.log(currentDay.format("dddd MMMM Do, YYYY"));
 async function getCityWeatherInfo(cityName) {
   return fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=fe6203bc0aed338120f1ad06f08effe6`
-    )
+  )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -27,7 +26,7 @@ async function getCityWeatherInfo(cityName) {
       // ATTEMPTING TO GET THE 5 DAY INFO AT THE SAME TIME BUT AM DOING IT WRONG.
       getForecast(cityName);
     });
-  }
+}
 
 const searchBtn = $("#searchBtn");
 const cityInput = $("#cityInput");
@@ -37,33 +36,31 @@ searchBtn.click(function () {
   console.log("City searched for: " + cityValue);
 });
 
-
 // FUNCTION TO GET THE 5 DAY FORECAST. ITS BROKEN BUT ON TRACK I THINK. NOT SURE HOW TO FETCH LOCATION.
- function getForecast(cityName) {
-   fetch(
-     `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=eae798b284ccde933e01fec4c703c7d6`
-   )
-     .then((response) => response.json())
-     .then((data) => {
-       console.log(data);
-       console.log("Forecast API fetch works")
-       $("#day1, #day2, #day3, #day4, #day5").text(`${data.name} ${data.main}°`);
-       console.log("Forecast displayed");
-       console.log(feelsLike)
-     });
- }
+function getForecast(cityName) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=eae798b284ccde933e01fec4c703c7d6`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log("Forecast API fetch works");
+      $("#day1, #day2, #day3, #day4, #day5").text(`${data.name} ${data.main}°`);
+      console.log("Forecast displayed");
+      console.log(feelsLike);
+    });
+}
 const day1 = $("#day1");
 const day2 = $("#day2");
 const day3 = $("#day3");
 const day4 = $("#day4");
 const day5 = $("#day5");
 
-
 // function works but im pulling from the API wrong.
 function leftBtnsSearch() {
   return fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=atlanta&lat=33.753746&lon=-84.386330&units=imperial&appid=fe6203bc0aed338120f1ad06f08effe6`
-    )
+  )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -73,7 +70,7 @@ function leftBtnsSearch() {
       $("#windSpeed").text(`Wind Speed: ${data.wind.speed}mph`);
       $("#description").text(`Sky: ${data.weather[0].main}`);
       const atlanta = $("#cityBtn1");
-      const denver= $("#cityBtn2");
+      const denver = $("#cityBtn2");
       const seattle = $("#cityBtn3");
       const sanFrancisco = $("#cityBtn4");
       const orlando = $("#cityBtn5");
